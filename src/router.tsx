4292b90d424f5a3c4d52aec2666b5bd066a7a815
pablo-caparelli <pablocaparelli@gmail.com>
2025-10-22 18:54:26 -0300

    Primer commit

---
+++ src/router.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppLayout from "@/layouts/AppLayout";
+import DashboardViews from "@/views/DashboardViews";
+import CreateProjectView from "./views/projects/CreateProjectView";
+import EditProjectView from "./views/projects/EditProjectView";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<DashboardViews />} index />
+          <Route path="/projects/create" element={<CreateProjectView />} />
+          <Route
+            path="/projects/:projectId/edit"
+            element={<EditProjectView />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
